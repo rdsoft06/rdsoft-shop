5e4fe9c287cc12aaa75fa64a9adaa0068baac217
Леонид Борисович Рыбаков <40237326+rdsoft06@users.noreply.github.com>
2019-11-11 20:38:56 +0200

    Update AccountPageAddresses.jsx

--- src/components/account/AccountPageAddresses.jsx
+++ src/components/account/AccountPageAddresses.jsx
@@ -14,7 +14,7 @@ export default function AccountPageAddresses() {
     const addresses = dataAddresses.map((address) => (
         <React.Fragment key={address.id}>
             <div className="addresses-list__item card address-card">
-                {address.default && <div className="address-card__badge">Default</div>}
+                {address.default && <div className="address-card__badge"><FormattedMessage id="acount.Default" defaultMessage="Default" /></div>}
 
                 <div className="address-card__body">
                     <div className="address-card__name">{`${address.firstName} ${address.lastName}`}</div>
@@ -28,17 +28,17 @@ export default function AccountPageAddresses() {
                         {address.address}
                     </div>
                     <div className="address-card__row">
-                        <div className="address-card__row-title">Phone Number</div>
+                        <div className="address-card__row-title"><FormattedMessage id="checkout.Phone" defaultMessage="Phone Number" /></div>
                         <div className="address-card__row-content">{address.phone}</div>
                     </div>
                     <div className="address-card__row">
-                        <div className="address-card__row-title">Email Address</div>
+                        <div className="address-card__row-title"><FormattedMessage id="checkout.Email_address" defaultMessage="Email Address" /></div>
                         <div className="address-card__row-content">{address.email}</div>
                     </div>
                     <div className="address-card__footer">
-                        <Link to="/">Edit</Link>
+                        <Link to="/"><FormattedMessage id="acount.Edit" defaultMessage="Edit" /></Link>
                         &nbsp;&nbsp;
-                        <Link to="/">Remove</Link>
+                        <Link to="/"><FormattedMessage id="acount.Remove" defaultMessage="Remove" /></Link>
                     </div>
                 </div>
             </div>
@@ -54,7 +54,7 @@ export default function AccountPageAddresses() {
 
             <Link to="/" className="addresses-list__item addresses-list__item--new">
                 <div className="addresses-list__plus" />
-                <div className="btn btn-secondary btn-sm">Add New</div>
+                <div className="btn btn-secondary btn-sm"><FormattedMessage id="acount.Add_New" defaultMessage="Add New" /></div>
             </Link>
             <div className="addresses-list__divider" />
             {addresses}
